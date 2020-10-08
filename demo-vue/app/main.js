aa707574d32a32425797dc5ebea48f869b42b2f1
Luigui Delyer <git@s1x.com.br>
2020-10-07 21:35:48 -0300

    fix names of files, include npm organization in package.json, change layout and colors of layout

--- demo-vue/app/main.js
+++ demo-vue/app/main.js
@@ -1,16 +1,22 @@
 import Vue from 'nativescript-vue'
 import App from './components/App'
 
-
-Vue.registerElement('YoonitCamera', () => require('nativescript-yoonit-camera').YoonitCamera)
-
-
-
 // Prints Vue logs when --env.production is *NOT* set while building
 Vue.config.silent = (TNS_ENV === 'production')
 
+Vue.registerElement(
+  'YoonitCamera',
+  () => require('@yoonit/nativescript-camera')
+    .YoonitCamera
+)
 
 new Vue({
-
-  render: h => h('frame', [h(App)])
-}).$start()
+  render: h =>
+    h(
+      'frame',
+      [
+        h(App)
+      ]
+    )
+})
+  .$start()
