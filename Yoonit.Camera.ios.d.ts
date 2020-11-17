2f88ae1396230ab5a89912fa9af09593a729367e
HaroldoTeruya <haroldo.s.teruya@gmail.com>
2020-11-17 16:48:06 -0300

    npm publish

---
+++ Yoonit.Camera.ios.d.ts
@@ -0,0 +1,20 @@
+import { CameraBase } from './Yoonit.Camera.common';
+export declare class YoonitCamera extends CameraBase {
+    nativeView: CameraView;
+    private permission;
+    createNativeView(): Object;
+    initNativeView(): void;
+    disposeNativeView(): void;
+    startCapture(captureType: string): void;
+    setFaceNumberOfImages(faceNumberOfImages: number): void;
+    setFaceDetectionBox(faceDetectionBox: boolean): void;
+    setFaceTimeBetweenImages(faceTimeBetweenImages: number): void;
+    setFacePaddingPercent(facePaddingPercent: number): void;
+    setFaceImageSize(width: number, height: number): void;
+    setFaceCaptureMinSize(faceCaptureMinSize: number): void;
+    setFaceCaptureMaxSize(faceCaptureMaxSize: number): void;
+    setFrameNumberOfImages(frameNumberOfImages: number): void;
+    setFrameTimeBetweenImages(frameTimeBetweenImages: number): void;
+    requestPermission(explanation?: string): Promise<boolean>;
+    hasPermission(): boolean;
+}
