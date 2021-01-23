dc9c467f470d8cb632609a91b077a87abf7de765
HaroldoTeruya <haroldo.s.teruya@gmail.com>
2021-01-23 08:53:32 -0300

    npm publish

---
+++ Yoonit.Camera.android.js
@@ -0,0 +1,171 @@
+import { CameraBase } from './Yoonit.Camera.common';
+import * as permissions from 'nativescript-permissions';
+import { ImageSource, File } from '@nativescript/core';
+import Validator from "./helpers/Validator";
+const { ValidateProps, Required, NativeMethod, } = Validator;
+const CAMERA = () => android.Manifest.permission.CAMERA;
+export class YoonitCamera extends CameraBase {
+    createNativeView() {
+        this.nativeView = new ai.cyberlabs.yoonit.camera.CameraView(this._context);
+        this.nativeView.setCameraEventListener(CameraEventListener.initWithOwner(new WeakRef(this)));
+        return this.nativeView;
+    }
+    initNativeView() {
+        this.nativeView.owner = this;
+        super.initNativeView();
+        Validator.PropMap.forEach((prop) => {
+            if (this.nativeView[prop.name]) {
+                this.nativeView[prop.name](prop.value);
+            }
+        });
+        Validator.PropMap = [];
+    }
+    disposeNativeView() {
+        this.nativeView.stopCapture();
+        this.nativeView.owner = null;
+        super.disposeNativeView();
+    }
+    requestPermission(explanation = '') {
+        return new Promise((resolve, reject) => permissions
+            .requestPermission(CAMERA(), explanation)
+            .then(() => resolve(true))
+            .catch(err => reject(false)));
+    }
+    hasPermission() {
+        return permissions.hasPermission(CAMERA());
+    }
+    setImageCaptureColorEncoding(colorEncoding) {
+        this.nativeView.setColorEncodingCapture(colorEncoding);
+    }
+}
+__decorate([
+    ValidateProps('colorEncoding', ['RGB', 'YUV']),
+    NativeMethod('setColorEncodingCapture'),
+    __param(0, Required),
+    __metadata("design:type", Function),
+    __metadata("design:paramtypes", [String]),
+    __metadata("design:returntype", void 0)
+], YoonitCamera.prototype, "setImageCaptureColorEncoding", null);
+var CameraEventListener = /** @class */ (function (_super) {
+    __extends(CameraEventListener, _super);
+    function CameraEventListener(owner) {
+        var _this = _super.call(this) || this;
+        _this.owner = owner;
+        // Required by Android runtime when native class is extended through TypeScript.
+        return global.__native(_this);
+    }
+    CameraEventListener_1 = CameraEventListener;
+    CameraEventListener.initWithOwner = function (owner) {
+        return new CameraEventListener_1(owner);
+    };
+    CameraEventListener.prototype.imageProcessing = function (imagePath) {
+        var source = ImageSource.fromFileSync(imagePath);
+        var imageFile = File.fromPath(imagePath);
+        var binary = imageFile.readSync();
+        return {
+            path: imagePath,
+            source: source,
+            binary: binary
+        };
+    };
+    CameraEventListener.prototype.onImageCaptured = function (type, count, total, imagePath, inferences) {
+        var owner = this.owner.get();
+        if (owner && !!imagePath) {
+            var image = this.imageProcessing(imagePath);
+            owner.notify({
+                eventName: 'imageCaptured',
+                object: owner,
+                type: type,
+                count: count,
+                total: total,
+                image: image
+            });
+        }
+    };
+    CameraEventListener.prototype.onFaceDetected = function (x, y, width, height) {
+        var owner = this.owner.get();
+        if (owner) {
+            owner.notify({
+                eventName: 'faceDetected',
+                object: owner,
+                x: x,
+                y: y,
+                width: width,
+                height: height
+            });
+        }
+    };
+    CameraEventListener.prototype.onFaceUndetected = function () {
+        var owner = this.owner.get();
+        if (owner) {
+            owner.notify({
+                eventName: 'faceDetected',
+                object: owner,
+                x: null,
+                y: null,
+                width: null,
+                height: null
+            });
+        }
+    };
+    CameraEventListener.prototype.onEndCapture = function () {
+        var owner = this.owner.get();
+        if (owner) {
+            owner.notify({
+                eventName: 'endCapture',
+                object: owner,
+            });
+        }
+    };
+    CameraEventListener.prototype.onQRCodeScanned = function (content) {
+        var owner = this.owner.get();
+        if (owner) {
+            owner.notify({
+                eventName: 'qrCodeContent',
+                object: owner,
+                content: content
+            });
+        }
+    };
+    CameraEventListener.prototype.onError = function (error) {
+        var owner = this.owner.get();
+        if (owner) {
+            owner.notify({
+                eventName: 'status',
+                object: owner,
+                status: {
+                    type: 'error',
+                    status: error
+                }
+            });
+        }
+    };
+    CameraEventListener.prototype.onMessage = function (message) {
+        var owner = this.owner.get();
+        if (owner) {
+            owner.notify({
+                eventName: 'status',
+                object: owner,
+                status: {
+                    type: 'message',
+                    status: message
+                }
+            });
+        }
+    };
+    CameraEventListener.prototype.onPermissionDenied = function () {
+        var owner = this.owner.get();
+        if (owner) {
+            owner.notify({
+                eventName: 'permissionDenied',
+                object: owner,
+            });
+        }
+    };
+    var CameraEventListener_1;
+    CameraEventListener = CameraEventListener_1 = __decorate([
+        Interfaces([ai.cyberlabs.yoonit.camera.interfaces.CameraEventListener])
+    ], CameraEventListener);
+    return CameraEventListener;
+}(java.lang.Object));
+//# sourceMappingURL=Yoonit.Camera.android.js.map
---
+++ index.d.ts
@@ -0,0 +1,74 @@
+// +-+-+-+-+-+-+
+// |y|o|o|n|i|t|
+// +-+-+-+-+-+-+
+//
+// +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+// | Yoonit Camera Plugin for NativeScript applications              |
+// | Luigui Delyer, Haroldo Teruya,                                  |
+// | Victor Goulart & Márcio Bruffato @ Cyberlabs AI 2020            |
+// +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+
+import {
+  ContentView,
+  EventData
+} from '@nativescript/core';
+
+export interface ImageCapturedEventData extends EventData {
+    type: string;
+    count: number;
+    total: number;
+    image: any;
+}
+
+export interface FaceDetectedEventData extends EventData {
+    x: number;
+    y: number;
+    width: number;
+    height: number;
+}
+
+export interface QRCodeScannedEventData extends EventData {
+    content: string;
+}
+
+export interface StatusEventData extends EventData {
+    status: any;
+}
+
+export declare class Camera extends ContentView {
+    requestPermission(explanationText?: string): Promise<boolean>;
+    hasPermission(): boolean;
+    preview(): void;
+    stopCapture(): void;
+    toggleLens(): void;
+    setCameraLens(lens: string): void;
+    getLens(): string;
+    startCapture(type: string): void;
+
+    setImageCapture(enable: boolean): void;
+    setImageCaptureAmount(numberOfImages: number): void;
+    setImageCaptureInterval(milliseconds: number): void;
+    setImageCaptureWidth(percentage: number): void;
+    setImageCaptureHeight(percentage: number): void;
+    setImageCaptureColorEncoding(colorEncoding: string): void;
+
+    setFacePaddingPercent(percentage: string): void;
+    setFaceCaptureMinSize(percentage: string): void;
+    setFaceCaptureMaxSize(percentage: string): void;
+    setFaceDetectionBox(enable: boolean): void;
+
+    setFaceROIEnable(enable: boolean): void;
+    setFaceROITopOffset(percentage): void;
+    setFaceROIRightOffset(percentage): void;
+    setFaceROIBottomOffset(percentage): void;
+    setFaceROILeftOffset(percentage): void;
+    setFaceROIMinSize(percentage: string): void;
+
+    on(eventNames: string, callback: (data: EventData) => void, thisArg?: any);
+    on(event: imageCaptured, callback: (args: ImageCapturedEventData) => void, thisArg?: any);
+    on(event: faceDetected, callback: (args: FaceDetectedEventData) => void, thisArg?: any);
+    on(event: endCapture, callback: () => void, thisArg?: any);
+    on(event: qrCodeContent, callback: (args: QRCodeScannedEventData) => void, thisArg?: any);
+    on(event: status, callback: (args: MessageEventData) => void, thisArg?: any);
+    on(event: permissionDenied, callback: () => void, thisArg?: any);
+}
