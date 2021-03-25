c6b7b82facf6461b3364864e298426c393150f89
Gabriel Rizzo <gabriel96.gsr@gmail.com>
2021-03-25 14:06:17 -0300

    refactoring(android.d.ts): Changing Roi and face methods names
    Changing function names by removing 'face' from it's names. Now they're generic. Also, removing faceROIMinSize.
    
    Breaking Changes

--- src/typings/android.d.ts
+++ src/typings/android.d.ts
@@ -33,24 +33,23 @@ declare namespace ai {
 					public setOutputImageHeight(param0: number): void;
 					public setSaveImageCaptured(param0: boolean): void;
 					public setColorEncodingCapture(param0: string): void;
-					public setFaceDetectionBox(param0: boolean): void;
+					public setDetectionBox(param0: boolean): void;
 					public setFacePaddingPercent(param0: number): void;
-					public setFaceCaptureMinSize(param0: number): void;
-					public setFaceCaptureMaxSize(param0: number): void;
-					public setFaceROIEnable(param0: boolean): void;
-					public setFaceROITopOffset(param0: number): void;
-					public setFaceROIRightOffset(param0: number): void;
-					public setFaceROIBottomOffset(param0: number): void;
-					public setFaceROILeftOffset(param0: number): void;
-					public setFaceROIMinSize(param0: number): void;
-					public setFaceROIAreaOffsetColor(
+					public setDetectionMinSize(param0: number): void;
+					public setDetectionMaxSize(param0: number): void;
+					public setROI(param0: boolean): void;
+					public setROITopOffset(param0: number): void;
+					public setROIRightOffset(param0: number): void;
+					public setROIBottomOffset(param0: number): void;
+					public setROILeftOffset(param0: number): void;
+					public setROIAreaOffsetColor(
 						param0: number,
 						param1: number,
 						param2: number,
 						param3: number
 					): void;
-					public setFaceROIAreaOffset(param0: boolean): void;
-					public setFaceROIEnable(param0: boolean): void;
+					public setROIAreaOffset(param0: boolean): void;
+					public setROIEnable(param0: boolean): void;
 					public setFaceContours(param0: boolean): void;
 					public setFaceContoursColor(...params: number): void;
 					public setComputerVision(param0: boolean): void;
